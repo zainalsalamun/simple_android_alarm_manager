ad15424387c0c166e4ad0c0dc52c5520e4ccfe74
zainalsalamun <zainalrtf@gmail.com>
2022-01-19 10:45:42 +0700

    init commit

---
+++ android/app/src/main/kotlin/com/example/simple_android_alarm_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.simple_android_alarm_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,93 @@
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_android_alarm_manager/utils/background_service.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({required this.title});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final BackgroundService _service = BackgroundService();
+
+  @override
+  void initState() {
+    super.initState();
+    port.listen((_) async => await _service.someTask());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: Text(
+                'Alarm with Delayed (Once)',
+              ),
+              onPressed: () async {
+                await AndroidAlarmManager.oneShot(
+                  Duration(seconds: 5),
+                  1,
+                  BackgroundService.callback,
+                  exact: true,
+                  wakeup: true,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text(
+                'Alarm with Date and Time (Once)',
+              ),
+              onPressed: () async {
+                await AndroidAlarmManager.oneShotAt(
+                  DateTime.now().add(Duration(seconds: 5)),
+                  2,
+                  BackgroundService.callback,
+                  exact: true,
+                  wakeup: true,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text(
+                'Alarm with Periodic',
+              ),
+              onPressed: () async {
+                await AndroidAlarmManager.periodic(
+                  Duration(minutes: 1),
+                  3,
+                  BackgroundService.callback,
+                  startAt: DateTime.now(),
+                  exact: true,
+                  wakeup: true,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              child: Text(
+                'Cancel Alarm by Id',
+              ),
+              onPressed: () async {
+                await AndroidAlarmManager.cancel(3);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
